7ff608dac6bee5cc569ce1d2a535327c43301f6b
Edwin Stefano Ibañez Bazan <esib_94@hotmail.com>
2019-09-20 20:59:33 -0300

    prueba1

---
+++ Practica1/Practica1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 0; i <= 5; i++)
+                for (j = 0; j <= 20; j = j + 2)
+                {
+                    if ((i == 1) && (j >= 7))
+                        goto salida;
+                    Console.WriteLine("i vale {0} y j vale {1}.", i, j);
+                }
+            salida:
+            Console.WriteLine("Fin del programa");
+        }
+    }
+}
